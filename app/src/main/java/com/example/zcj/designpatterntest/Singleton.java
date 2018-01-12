5309f057aef4247b2e2db6af70eaf630dc61fdb3
zengchengjie <zengchengjieit@163.com>
2018-01-12 17:24:43 +0800

    添加另一种单例

--- app/src/main/java/com/example/zcj/designpatterntest/Singleton.java
+++ app/src/main/java/com/example/zcj/designpatterntest/Singleton.java
@@ -8,7 +8,7 @@ public class Singleton{
 		if(incetance ==null){
 			incetance = new Singleton();
 		}
-			return incetance;
+		return incetance;
 	}
 }
 
@@ -26,24 +26,37 @@ public class Singleton {
 /*饿汉式 比较常用，但是容易产生垃圾对象
   优点：没有加锁，执行效率会提高
   缺点：类加载时就初始化，浪费内存*/
-public class Singleton {
-	private static Singleton instance = new Singleton();
-	private Singleton(){}
-	public static Singleton getIncetance(){
-		return instance;
-	}
-}
+  public class Singleton {
+  	private static Singleton instance = new Singleton();
+  	private Singleton(){}
+  	public static Singleton getIncetance(){
+  	}
+  }
 //双重校验锁 实现难度较为复杂 getIncetance的性能对应用程序很关键
 //使用双重锁的方式，在多线程的情况下能够保持高性能
-public class Singleton {
-	private static Singleton incetance;
-	private Singleton(){}
-	public static Singleton getIncetance{
-		if (instance==null) {
-			synchronized(Singleton.class){
-				instance = new Singleton();
-			}
-			return instance;
-		}
-	}
-}
+  public class Singleton {
+  	private static Singleton incetance;
+  		return instance;
+  	private Singleton(){}
+  	public static Singleton getIncetance{
+  		if (instance==null) {
+  			synchronized(Singleton.class){
+  				instance = new Singleton();
+  			}
+  			return instance;
+  		}
+  	}
+  }
+  //登记式/静态内部类
+  //实现简单 达到和双检锁一样的功效 对静态域使用延迟初始化，
+  //应使用这种方式而不是双检锁方式。这种方式只适用于静态域的情况，
+  //双检锁方式可在实例域需要延迟初始化时使用
+  public class Singleton{
+  	private static class SingletonHolder{
+  		private static final Singleton INCETANCE = new Singleton();
+  	}
+  	private Singleton();
+  	public static final Singleton getIncetance(){
+  		return SingletonHolder.INCETANCE;
+  	}
+  }
